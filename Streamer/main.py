1b670c5bd8d57ba46ee46811e882069b0506f7f2
ehud <ehud.plaksin@gmail.com>
2019-05-02 20:06:23 +0300

    proof of concept, now works more or less hopefully

--- Streamer/main.py
+++ Streamer/main.py
@@ -1,54 +1,60 @@
 #! /usr/bin/env python
 from scapy.all import *
+import data_factory
+from SegmentContainerManipulation import add_sessions_by_server
+from SegmentContainerManipulation import add_sessions_by_time
 
-#holds the streams in the 5 seconds interval.
-streams = {}
 
-#quaduple that represent a video-related stream.
-ott_streams = []
 
-#list of pcap file name's.
-cuttings = []
-stream_cuttings = []
-connenction_open_times = []
-#TCP flags
-FIN = 0x01
-SYN = 0x02
-RST = 0x04
-PSH = 0x08
-ACK = 0x10
-URG = 0x20
-ECE = 0x40
-CWR = 0x80
-
+client_ip = '10.185.33.245'
 original_pcap = rdpcap("iphone7_auto_04_19_id_1.pcap")
+real_start_time = original_pcap[0].time
 start_time = original_pcap[0].time
+end_time = original_pcap[-1].time
 original_pcap = None
 
-my_dir = os.listdir("cuttings")
-for pcap_file_path in my_dir:
-    stream_cuttings.append(cuttings)
-    cuttings = []
-    for pcap_stream_cuttings_files in os.listdir("cuttings\\" + pcap_file_path):
-        if pcap_stream_cuttings_files.startswith('out'):
-            cuttings.append(rdpcap("cuttings\\" + pcap_file_path + "\\" + pcap_stream_cuttings_files))
+time_interval = end_time - start_time
 
-    for stream in stream_cuttings:
-        if len(stream) > 0:
-            if len(stream[0]) > 0:
-                print((stream[0][0].time - start_time))
-        for pcap in stream:
-            total_payloads = 0
-            if len(pcap) > 0:
-                pkt_key = [pcap[0][IP].src, pcap[0].sport, pcap[0][IP].dst, pcap[0].dport]
-            for pkt in pcap:
-                total_payloads += len(pkt)
-                #Bingo!
-                if total_payloads > 100000:
-                    if pkt_key not in ott_streams:
-                        ott_streams.append(pkt_key)
-                    # pkt_key[3] = 443
-                    # if pkt_key not in ott_streams:
-                    #     ott_streams.append(pkt_key)
-        streams = {}
-print(ott_streams)
+unclassified_streams = data_factory.produce()
+total_payloads = 0
+video_treshold = 100000
+bingo = True
+video_related_streams = set([])
+
+
+# for index, stream in enumerate(streams):
+#     if len(stream.curr_splinter) > 0:
+#         print(stream.curr_splinter[0].time - start_time)
+#     else:
+#         print(stream.splinters_path)
+
+for x in range(int(time_interval / 5)):
+    for index, stream in enumerate(unclassified_streams):
+        total_payloads = 0
+        flag = 0
+        while flag <= 1:
+            curr_stream = stream.curr_splinter
+            if curr_stream is None:
+                flag = 2
+                continue
+            # print(stream.to_string(), curr_stream[0].time - start_time, 'bla')
+            if abs(curr_stream[0].time - start_time) <= 5:
+
+                for packet in data_factory.getdownstream(curr_stream, client_ip):
+                    if (packet.time >= start_time) and (packet.time <= start_time + 5):
+                        total_payloads += len(packet)
+
+                if total_payloads >= video_treshold:
+                    video_related_streams.add(stream.to_string())
+                    add_sessions_by_server(unclassified_streams, stream, video_related_streams)
+                    add_sessions_by_time(unclassified_streams, stream, video_related_streams)
+                    unclassified_streams.remove(stream)
+                    flag = 2
+
+            if curr_stream[0].time - start_time <= 0:
+                stream.next()
+            flag += 1
+    start_time += 5
+
+for stream_string in video_related_streams:
+    print(stream_string)
