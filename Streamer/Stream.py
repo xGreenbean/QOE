cbfd00aca9239fcda946105fcb97641e5617bc76
ehud <ehud.plaksin@gmail.com>
2019-05-02 20:03:49 +0300

    changed to_string() moved error handling of null packets to be handled in the preproccesing phase

--- Streamer/Stream.py
+++ Streamer/Stream.py
@@ -1,15 +1,18 @@
-from scapy.all import *
+
 class Stream(object):
+    def __init__(self, ip_src, ip_dst, port_src, port_dst, connection_opening_time):
+        self.ip_src = ip_src
+        self.ip_dst = ip_dst
+        self.port_src = port_src
+        self.port_dst = port_dst
+        self.connection_opening_time = connection_opening_time
 
-    def __init__(self, splinters_path):
-        self.splinters_path = splinters_path
-        self.splinters_it = iter(splinters_path)
-        self.curr_splinter = rdpcap(next(self.splinters_it))
+    def to_same_server_over_port_443(self, stream_other):
+        return (self.ip_src == stream_other.ip_src and self.ip_dst == stream_other.ip_dst
+                and stream_other.port_dst == 443)
 
-    def next(self):
-        path = next(self.splinters_it, None)
-        if path is None:
-            return None
-        else:
-            self.curr_splinter = rdpcap(path)
-            return self.curr_splinter
+    def get_start_time(self):
+        return self.connection_opening_time
+
+    def to_string(self):
+        return "'" + self.ip_src + "' " + str(self.port_src) + " '" +  self.ip_dst + "' " + str(self.port_dst)
