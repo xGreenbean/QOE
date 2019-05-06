83f74d09171fa1822bcc3f91affb90e4e18bde4e
ehud <ehud.plaksin@gmail.com>
2019-05-06 19:17:14 +0300

    added some dataset functions

--- Streamer/build_data_set.py
+++ Streamer/build_data_set.py
@@ -1,7 +1,9 @@
 import os
 global splitcap_path
-data_set_path = "C:\Users\ehud\Desktop\dataset"
-splitcap_path = "C:\Users\ehud\Desktop\SplitCap_2-1\Splitcap.exe"
+
+# data_set_path = "C:\Users\ehud\Desktop\dataset"
+data_set_path = "/home/cyberlab/Desktop/dataset"
+splitcap_path = "C:\\Users\\ehud\\Desktop\\SplitCap_2-1\\Splitcap.exe"
 
 #add sessions dir to directories
 def add_sessions_dir(data_set_path):
@@ -9,7 +11,7 @@ def add_sessions_dir(data_set_path):
         for subdirs in os.listdir(os.path.join(data_set_path,dirs)):
             if os.path.isdir(os.path.join(os.path.join(data_set_path, dirs, subdirs))):
                 if 'sessions' in os.listdir(os.path.join(data_set_path, dirs, subdirs)):
-                    print os.path.join(data_set_path, dirs, subdirs)
+                    print (os.path.join(data_set_path, dirs, subdirs))
                 else:
                     os.mkdir(os.path.join(data_set_path, dirs, subdirs,'sessions'))
 
@@ -29,3 +31,22 @@ def fill_sessions_dir(data_set_path):
                                 curr_pcap_path = os.path.join(curr_dir,pcap_file)
                                 os.system(splitcap_path + ' -r ' + '"' + curr_pcap_path +
                                           '"' + ' -o ' + '"' + curr_path_sessions + '"')
+
+# create directory for every session and move it there
+def create_dirs_for_sessions(data_set_path):
+    for dirName, subdirList, fileList in os.walk(data_set_path):
+        if dirName.find('sessions') != -1:
+            # print('Found directory: %s' % dirName)
+            for fname in fileList:
+                if not os.path.exists(os.path.join(dirName, fname.split('.')[0])):
+                    os.mkdir(os.path.join(dirName, fname.split('.')[2]))
+                    os.rename(os.path.join(dirName, fname), os.path.join(dirName, fname.split('.')[2], fname))
+
+
+def split_sessions_by_time(data_set_path):
+    for dirName, subdirList, fileList in os.walk(data_set_path):
+        if dirName.find('TCP') != -1 or dirName.find('UDP') != -1:
+            for fname in fileList:
+                print(fname)
+
+split_sessions_by_time(data_set_path)
