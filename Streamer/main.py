6dcce6e3d4a61a90a91fe0600bb609ccf3727f60
ehud <ehud.plaksin@gmail.com>
2019-05-07 16:47:11 +0300

    Activity analasys now works

--- Streamer/main.py
+++ Streamer/main.py
@@ -3,58 +3,56 @@ from scapy.all import *
 import data_factory
 from SegmentContainerManipulation import add_sessions_by_server
 from SegmentContainerManipulation import add_sessions_by_time
+from Activity import Activity
+
+
+data_set_path = "/home/cyberlab/Desktop/dataset"
 
 
 
-client_ip = '10.185.33.245'
-original_pcap = rdpcap("iphone7_auto_04_19_id_1.pcap")
-real_start_time = original_pcap[0].time
-start_time = original_pcap[0].time
-end_time = original_pcap[-1].time
-original_pcap = None
-
-time_interval = end_time - start_time
-
-unclassified_streams = data_factory.produce()
-total_payloads = 0
-video_treshold = 100000
-bingo = True
-video_related_streams = set([])
 
 
-# for index, stream in enumerate(streams):
-#     if len(stream.curr_splinter) > 0:
-#         print(stream.curr_splinter[0].time - start_time)
-#     else:
-#         print(stream.splinters_path)
+activities = []
+activity_paths = data_factory.produce(data_set_path)
+for activity_path in activity_paths:
+    if activity_path.path.find('iphone youtube/id_4') != -1:
+        activities.append(Activity(activity_path))
 
-for x in range(int(time_interval / 5)):
-    for index, stream in enumerate(unclassified_streams):
-        total_payloads = 0
-        flag = 0
-        while flag <= 1:
-            curr_stream = stream.curr_splinter
-            if curr_stream is None:
-                flag = 2
-                continue
-            # print(stream.to_string(), curr_stream[0].time - start_time, 'bla')
-            if abs(curr_stream[0].time - start_time) <= 5:
+for activity in activities:
+    client_ip = activity.client_ip
+    real_start_time = activity.get_start_time()
+    start_time = activity.get_start_time()
+    end_time = activity.get_end_time()
+    time_interval = end_time - start_time
+    total_payloads = 0
+    video_treshold = 100000
+    video_related_streams = set([])
 
-                for packet in data_factory.getdownstream(curr_stream, client_ip):
-                    if (packet.time >= start_time) and (packet.time <= start_time + 5):
-                        total_payloads += len(packet)
-
-                if total_payloads >= video_treshold:
-                    video_related_streams.add(stream.to_string())
-                    add_sessions_by_server(unclassified_streams, stream, video_related_streams)
-                    add_sessions_by_time(unclassified_streams, stream, video_related_streams)
-                    unclassified_streams.remove(stream)
+    for x in range(int(time_interval / 5)):
+        for index, stream in enumerate(activity.streams):
+            total_payloads = 0
+            flag = 0
+            while flag <= 1:
+                curr_stream = stream.curr_splinter
+                if curr_stream is None:
                     flag = 2
+                    continue
+                if abs(curr_stream[0].time - start_time) <= 5:
+                    for packet in data_factory.getdownstream(curr_stream, client_ip):
+                        if (packet.time >= start_time) and (packet.time <= start_time + 5):
+                            total_payloads += len(packet)
 
-            if curr_stream[0].time - start_time <= 0:
-                stream.next()
-            flag += 1
-    start_time += 5
+                    if total_payloads >= video_treshold:
+                        video_related_streams.add(stream.to_string())
+                        add_sessions_by_server(activity.streams, stream, video_related_streams)
+                        add_sessions_by_time(activity.streams, stream, video_related_streams)
+                        activity.streams.remove(stream)
+                        flag = 2
 
-for stream_string in video_related_streams:
-    print(stream_string)
+                if curr_stream[0].time - start_time <= 0:
+                    stream.next()
+                flag += 1
+        start_time += 5
+
+    for stream_string in video_related_streams:
+        print(stream_string)
