430a52000b38fdf7883e13b9ef3a922c63ab1dd6
ehud <ehud.plaksin@gmail.com>
2019-05-07 12:09:24 +0300

    added more functions to build_data_set. now build_data_set suppourts building sessions and splits from pcap.

--- Streamer/data_set_manipulation/build_data_set.py
+++ Streamer/data_set_manipulation/build_data_set.py
@@ -1,9 +1,12 @@
 import os
 global splitcap_path
+import shutil
+from scapy.all import *
 
 # data_set_path = "C:\Users\ehud\Desktop\dataset"
 data_set_path = "/home/cyberlab/Desktop/dataset"
 splitcap_path = "C:\\Users\\ehud\\Desktop\\SplitCap_2-1\\Splitcap.exe"
+
 
 #add sessions dir to directories
 def add_sessions_dir(data_set_path):
@@ -15,7 +18,9 @@ def add_sessions_dir(data_set_path):
                 else:
                     os.mkdir(os.path.join(data_set_path, dirs, subdirs,'sessions'))
 
+
 #fill sessions directories with split cap
+#this only works in windows!!
 def fill_sessions_dir(data_set_path):
     for dirs in os.listdir(data_set_path):
         for subdirs in os.listdir(os.path.join(data_set_path,dirs)):
@@ -35,6 +40,7 @@ def fill_sessions_dir(data_set_path):
 # create directory for every session and move it there
 def create_dirs_for_sessions(data_set_path):
     for dirName, subdirList, fileList in os.walk(data_set_path):
+        #if there is no session directory
         if dirName.find('sessions') != -1:
             # print('Found directory: %s' % dirName)
             for fname in fileList:
@@ -43,10 +49,40 @@ def create_dirs_for_sessions(data_set_path):
                     os.rename(os.path.join(dirName, fname), os.path.join(dirName, fname.split('.')[2], fname))
 
 
-def split_sessions_by_time(data_set_path):
+#split each sessions by time, into the 'splits' directory
+def split_sessions_by_time(data_set_path, split_interval):
     for dirName, subdirList, fileList in os.walk(data_set_path):
-        if dirName.find('TCP') != -1 or dirName.find('UDP') != -1:
+        if dirName.split(os.sep)[-1].find('TCP') != -1 or dirName.split(os.sep)[-1].find('UDP') != -1:
             for fname in fileList:
-                print(fname)
+                if not os.path.exists(os.path.join(dirName, 'splits')):
+                    os.mkdir(os.path.join(dirName, 'splits'))
+                #check for exact path
+                pcap_file_path = os.path.join(dirName, fname)
+                out_path = os.path.join(dirName, 'splits', 'out.pcap')
+                # print(pcap_file_path)
+                # print(out_path)
+                if len(os.listdir(os.path.join(dirName, 'splits'))) == 0:
+                     os.system('editcap -i ' + str(split_interval) + ' ' + '"' + pcap_file_path + '"'
+                               ' "' + out_path + '"')
 
-split_sessions_by_time(data_set_path)
+
+#deletes the splits directory. we use it if we want to use diffrent time interval
+def clean_splits(data_set_path):
+    for dirName, subdirList, fileList in os.walk(data_set_path):
+        if dirName.find('splits') != -1:
+            shutil.rmtree(dirName)
+        for fname in fileList:
+            if fname.find('splits') != -1:
+                os.remove(os.path.join(dirName, fname))
+
+
+#deletes empty pcaps in the splits directories
+def delete_empty_pcaps(data_set_path):
+    for dirName, subdirList, fileList in os.walk(data_set_path):
+        if dirName.split(os.sep)[-1].find('splits') != -1:
+            for fname in fileList:
+                if len(rdpcap(os.path.join(dirName,fname))) == 0:
+                    os.remove(os.path.join(dirName,fname))
+
+
+
