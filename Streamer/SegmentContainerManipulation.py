3ec1ed4385b79665abc840c86a335b029ed5aa5c
ehud <ehud.plaksin@gmail.com>
2019-05-07 16:46:53 +0300

    refactored segmentContainerManipulation

--- Streamer/SegmentContainerManipulation.py
+++ Streamer/SegmentContainerManipulation.py
@@ -9,6 +9,5 @@ def add_sessions_by_server(streams, stream_video, video_streams):
 
 def add_sessions_by_time(streams, stream_video, video_streams):
     for stream in streams:
-        print('start times:', stream_video.get_start_time() - stream.get_start_time())
         if abs(stream_video.get_start_time() - stream.get_start_time()) < delta_t:
             video_streams.add(stream.to_string())
