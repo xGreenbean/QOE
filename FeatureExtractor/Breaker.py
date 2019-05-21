df60d2cce6f9e790170218b363b57073bc7f6615
ehud <ehud.plaksin@gmail.com>
2019-05-21 17:27:09 +0300

    quick commit, cleaner wants to kick me out

--- FeatureExtractor/Breaker.py
+++ FeatureExtractor/Breaker.py
@@ -1,5 +1,5 @@
 from containers.Session import *
-
+import pandas as pd
 
 class Breaker(object):
     def __init__(self, session, delta_t, threshold_t):
@@ -15,14 +15,14 @@ class Breaker(object):
             if row['ip.src'] == self.session.srcIp and row['frame.len'] > self.threshold_t and\
                     (request_start_time == 0 or row['frame.time_epoch'] - request_start_time > self.delta_t):
                 if len(request_response) > 0:
-                    requests_responses.append(request_response)
-                request_response = []
-
+                    requests_responses.append(list(request_response))
+                request_response.clear()
                 request_start_time = row['frame.time_epoch']
-
-            else:
                 request_response.append(row)
+            else:
+                if len(request_response) > 0:
+                    request_response.append(row)
 
         if len(request_response) > 0:
-            requests_responses.append(request_response)
+            requests_responses.append(pd.DataFrame(request_response,self.session.all_packets.columns))
         return requests_responses
--- FeatureExtractor/tests/test_breaker.py
+++ FeatureExtractor/tests/test_breaker.py
@@ -1,11 +1,24 @@
 from Breaker import *
 from containers.Interaction import *
+import os
 
-pcap_csv_path = '/home/cyberlab/Desktop/test/testw.csv'
+pcap_csv_path = '~/Desktop/QOE/test_source_filestest_breaker_1.csv'
+pcap_csv_label_path = '~/Desktop/QOE/test_source_filesbreaker_test_labels_1.csv'
 interaction = Interaction(pd.read_csv(pcap_csv_path))
+pd_labels = pd.read_csv(pcap_csv_label_path)
 
-for sess in interaction.get_sessions():
-    rrs_df = Breaker(sess, 0.1, 250).sess_break()
-    for rr_df in rrs_df:
-        print(len(rrs_df), rr_df[0].values)
-        break
+def test(pcap_csv_path, pcap_csv_label_path):
+    interaction = Interaction(pd.read_csv(pcap_csv_path))
+    pd_labels = pd.read_csv(pcap_csv_label_path)
+
+    for sess in interaction.get_sessions():
+        rrs_df = Breaker(sess, 0.05, 250).sess_break()
+        for rr_df in rrs_df:
+            get_number =rr_df[0]['frame.number']
+            if not (pd_labels['Info'][get_number - 1].startswith('GET') or
+                    pd_labels['Info'][get_number - 1].startswith('POST')):
+                print('test failed at:', pd_labels['Info'][get_number - 1])
+                return False
+    return True
+
+
