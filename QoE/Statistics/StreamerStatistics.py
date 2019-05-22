15c3de459257a63f0957aef45340e5b9f94da0fc
Ehud Plaksin <johndoe@example.com>
2019-05-22 23:46:46 +0300

    modified data factory, added sni function

--- QoE/Statistics/StreamerStatistics.py
+++
@@ -1,9 +0,0 @@
-""""""
-
-
-class StreamerStatistics:
-    def __init__(self):
-        self.caught_in_time = []
-        self.caught_in_server = []
-        # list of tuples (captured, capturer)
-        self.caught_in_threshold = []
