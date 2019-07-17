036c883c4bbd7cf982f6fbab0e8749acc8f3cdd8
ehud <ehud.plaksin@gmail.com>
2019-07-17 17:01:01 +0300

    pushing for report

--- src/Main.py
+++ src/Main.py
@@ -6,8 +6,12 @@ from features.FeatureAggregation import *
 from generators.datafactory import *
 from tools.Breaker import *
 from tools.Peaker import *
+import pandas as pd
 if __name__ == '__main__':
-    DataFactory.sessions_to_csv(breaker=True,path='sess_breaker.csv')
-    DataFactory.sessions_to_csv(peaker=True,path='sess_peaker')
-    DataFactory.bins_to_csv(breaker=True,path='bins_breaker.csv')
-    DataFactory.bins_to_csv(peaker=True,path='bin_speaker.csv')
+    # DataFactory.sessions_to_csv(breaker=True, path='sess_breaker.csv')
+    # DataFactory.sessions_to_csv(peaker=True, path='sess_peaker')
+    # DataFactory.bins_to_csv(breaker=True, path='bins_breaker.csv')
+    # DataFactory.bins_to_csv(peaker=True, path='bin_speaker.csv')
+    df = pd.read_csv('/home/cyberlab/Desktop/silhouette-trace/https-240p/https-2ktuvx9hrMw-240p-catnap.txt'
+                     ,header=None, usecols=[1, 14], names=[])
+    print(df.head())
--- src/configs/conf.py
+++ src/configs/conf.py
@@ -27,7 +27,7 @@ videolike = {
 
 
 """path to dataset"""
-dataset_path = '/home/ehud/Desktop/dataset'
+dataset_path = 'home/cyberlab/Desktop/dataset/'
 # ------------------------------------------------------------------------------------------------------------------
 """Breaker settings"""
 delta_t = 0.1
